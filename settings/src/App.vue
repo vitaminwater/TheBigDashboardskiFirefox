2d4225c4cbad48b6814d7a1cfe0f93cad516dfc3
vitaminwater <stantin04@gmail.com>
2019-02-12 15:44:21 +0100

    added basic settings vuejs app

--- settings/src/App.vue
+++ settings/src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <div v-if='!loading'>
+      <div v-for='(setting, name) in settings'>
+        {{ name }}: <input v-model="settings[name]">
+      </div>
+      <button @click="persist">Save</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+
+const DEFAULT_SETTINGS = {
+  screen: 0,
+  tab1: '',
+  tab2: '',
+  tab3: '',
+  tab4: '',
+}
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      loading: true,
+      settings: {},
+    }
+  },
+  async mounted() {
+    const stored = await browser.storage.local.get(Object.keys(DEFAULT_SETTINGS))
+    Object.keys(DEFAULT_SETTINGS).forEach((k) => {
+      this.$set(this.settings, k, stored[k] || DEFAULT_SETTINGS[k])
+    })
+    this.loading = false
+  },
+
+  methods: {
+    async persist() {
+      const store = {}
+      Object.keys(DEFAULT_SETTINGS).forEach((k) => {
+        console.log(this.settings[k])
+        store[k] = this.settings[k]
+      })
+      await browser.storage.local.set(store);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 </style>
